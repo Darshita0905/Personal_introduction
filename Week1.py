0b05c1b4b514dd082429dae88cd35e9865e263f7
Darshita <darshitasoni0905@gmail.com>
2025-10-17 02:49:36 +0530

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your favorite hobby: ")
+
+
+print("\n--- Personal Introduction ---")
+print(f"Hello {name}! 👋")
+print(f"You are {age} years old.")
+print(f"Your hobby is {hobby}. That sounds fun!")
+print("Welcome to the Personal Introduction Program! 😊")
